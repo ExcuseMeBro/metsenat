0cacdd2b1de616b00b1b4fb34ad426b4ce72f736
OG'ABEK <shomoyxhacker007@mail.ru>
2023-10-22 15:00:32 +0500

    feat: chart added

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { createPinia } from "pinia";
 import "@/global.css";
 import router from "@/router";
 import App from "@/App.vue";
+import VueApexCharts from 'vue3-apexcharts'
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(VueApexCharts);
 app.mount("#app");
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,10 +1,12 @@
 <script setup>
+import Chart from '../components/dashboard/Chart.vue';
 import Reports from '../components/dashboard/Reports.vue';
 
 
 </script>
 <template>
   <Reports />
+  <Chart />
 </template>
 
 
